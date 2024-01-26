08047bddc3bf394e5599cdbf57ab488ae536966f
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2024-01-26 15:14:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+// now to store browser data 
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// display data whenever browser is closed and re-opened again
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask()
